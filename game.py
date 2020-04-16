b8a2df533121d5e3bc1081b9b9296c8690cce83d
Fernando Funez <49736675+ferfunes@users.noreply.github.com>
2020-04-16 10:36:40 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 
-# -----------------------Board---------------------
+# -----------------------Board--------------------
 # Step 1: Write a function that can print out a board. Set up your board as a list, where each index 1-9 corresponds with a number on a number pad,
 # so you get a 3 by 3 board representation.
 #test
